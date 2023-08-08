d13e1f58132c1070972e38c943a47731064f4eba
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-08 20:39:35 +0600

    Squats.cpp

---
+++ Squats.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int S, A;
+        cin >> S;
+        A = S * 15;
+        cout << A << "\n";
+    }
+    return 0;
+}
