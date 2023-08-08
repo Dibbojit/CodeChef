b86818aca41d14daf7db917bae55e0aa9a7f2b05
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-08 22:11:55 +0600

    Donation Drive.cpp

---
+++ Donation Drive.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int X, Y, A;
+        cin >> X >> Y;
+        A = X - Y;
+        cout << A << "\n";
+    }
+    return 0;
+}
