b1fab9d034cdd36fbb27032bda11eecd6e68a934
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-08 21:05:06 +0600

    Biriyani classes.cpp

---
+++ Biriyani classes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int W, M, Total;
+        cin >> W >> M;
+        Total = W * M;
+        cout << Total << "\n";
+    }
+    return 0;
+}
