d4c44afa5e1cd7cebe1537e16ab92fa4e16020d0
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-10 22:03:06 +0600

    2000.cpp

---
+++ 2000.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    {
+        int notes, total, note2;
+        cin >> notes;
+        total = notes * 2000;
+        note2 = total / 500;
+        cout << note2 << endl;
+    }
+    return 0;
+    
+}
