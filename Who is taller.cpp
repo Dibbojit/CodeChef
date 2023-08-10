b34de21fa42f357c6895c1590286d8e1b70cb41f
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-10 18:30:05 +0600

    Who is taller.cpp

---
+++ Who is taller.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        if(A > B)
+        {
+            cout << "A" << endl;
+        }
+        else if(A < B)
+        {
+            cout << "B" << endl;
+        }
+    }
+    return 0;
+    
+}    
