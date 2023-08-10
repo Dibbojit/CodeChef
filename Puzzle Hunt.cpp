171d53ada206be326efcaed1f9f339a3df35953e
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-10 18:02:24 +0600

    Puzzle Hunt.cpp

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    {
+        int x;
+        cin >> x;
+        if(x == 6 || x == 7 || x == 8)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+    
+}    
