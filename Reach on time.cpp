95ef1b6c9a81003a54d4d4ea5fc673b90e417e7d
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-10 17:56:00 +0600

    Create Reach on time.cpp

---
+++ Reach on time.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int x;
+        cin >> x;
+        if(x >= 30)
+        {
+            cout << "YES" <<endl;
+        }
+        else 
+        {
+            cout << "NO" <<endl;
+        }
+    }
+    return 0;
+    
+}    
