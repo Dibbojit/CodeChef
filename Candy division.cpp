4e35375fd49016daa028e379b34ff50af8777f30
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-11 11:47:02 +0600

    division.cpp

---
+++ Candy division.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin >> n;
+while(n--)
+    {
+        int x;
+        cin >> x;
+        if(x % 3 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" <<endl;
+        }
+    }   
+    return 0;
+    
+}
