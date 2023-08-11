2987d220cbefe203bee4fc3c4399975a51c4d605
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-11 12:30:00 +0600

    Ageing.cpp

---
+++ Ageing.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin >> n;
+while(n--)
+    {
+        int chef, chefina;
+        cin >> chef;
+        chefina = chef - 10;
+        cout << chefina << endl;
+    }        
+    return 0;
+    
+}
