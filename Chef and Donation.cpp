4a2958d747ee85d0e5449e58d03f7e23d0f56d26
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-12 22:11:31 +0600

    Chef and Donation.cpp

---
+++ Chef and Donation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin >> n;
+while(n--)
+    {
+        int chef, chefina, sub;
+        cin >> chef >> chefina;
+        sub = chefina - chef;
+        cout << sub << endl;
+    }        
+    return 0;
+    
+}
