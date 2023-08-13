fe8262563a4627e85c0cd3b5a4687d1a793b1e22
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-13 16:34:49 +0600

    learning sql.cpp

---
+++ learning sql.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+        int row, column, extra;
+        cin >> row >> column >> extra;
+
+        cout << (row + extra)* column << endl;
+        return 0;
+    }
+    
