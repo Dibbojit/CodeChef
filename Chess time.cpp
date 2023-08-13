a93df51bffec862b8ec0938f29d64158b49bcc3d
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-13 15:41:36 +0600

    Chess time.cpp

---
+++ Chess time.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        n = (n * 60) / 20;
+        cout << n << endl;
+    }
+    return 0;
+}
