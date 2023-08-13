18302354c51e2171d50790d2f4a10bd98031b45d
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-13 15:17:33 +0600

    Rainfall.cpp

---
+++ Rainfall.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x;
+        cin >> x;
+
+        if(x < 3)
+        {
+            cout << "LIGHT" << endl;
+        }
+        else if(x >= 3 && x < 7)
+        {
+            cout << "MODERATE" << endl;
+        }
+        else if(x >= 7)
+        {
+            cout << "HEAVY" << endl;
+        }
+    }
+    return 0;
+}
