a0093c0b39fe5a83876713ba3817a7ee807985c7
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 10:23:34 +0600

    Sleep deprivation.cpp

---
+++ Sleep deprivation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int sleep;
+        cin >> sleep;
+        if(sleep >= 7)
+        {
+            cout << "No" << endl;
+        }
+        else
+        {
+            cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
+    
