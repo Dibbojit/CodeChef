4a6eb2eb49283ea99faacece328e6a78b7cb6a26
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 10:10:26 +0600

    AB difference.cpp

---
+++ AB difference.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+ 
+        cout << abs((A * B) - (A + B)) << endl;
+  
+ return 0;
+}
+    
