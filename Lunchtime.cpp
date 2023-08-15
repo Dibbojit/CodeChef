9ebec3ff41034543bd3eecaa63d6c8d2c6ffb4da
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 11:12:30 +0600

    Lunchtime.cpp

---
+++ Lunchtime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int time;
+        cin >> time;
+        if(time >= 1 && time <= 4)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+    
