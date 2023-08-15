bcb1f4166fd594a414ef8ee0df39c5e565d4977c
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 09:23:18 +0600

    Podium finish.cpp

---
+++ Podium finish.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int t;
+cin >> t;
+while(t--)
+{
+    int first, second;
+    cin >> first >> second;
+    first = first + second;
+    cout << first << endl;
+}
+ return 0;
+}
+    
