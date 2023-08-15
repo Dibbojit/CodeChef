0d272808eafc84ce4dbdc92934d5532419a95745
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 09:43:34 +0600

    Couple game.cpp

---
+++ Couple game.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int t;
+cin >> t;
+while(t--)
+{
+    int girls, boys;
+    cin >> girls >> boys;
+    cout << boys - girls << endl;
+}
+ return 0;
+}
+    
