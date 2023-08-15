22ce840b50f8efec1c2e11d36dd4dee2636359a1
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-15 11:10:20 +0600

    Water requirement.cpp

---
+++ Water requirement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int liters;
+        cin >> liters;
+        cout << liters * 2 << endl;
+    }
+    return 0;
+}
+    
