cb1b01e305d317d5b6acf6844e4da5bde8e125ab
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-19 20:16:52 +0600

    Oneful pair.cpp

---
+++ Oneful pair.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+    
+        int a, b, c;
+        cin >> a >> b;
+        c = a + b + (a * b);
+        if(c == 111)
+        {
+            cout << "Yes" << endl;
+        }
+        else 
+        {
+            cout << "No" << endl;
+        }
+    
+    return 0;
+}
