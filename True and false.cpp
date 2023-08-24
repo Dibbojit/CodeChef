ba32513ff2d1b1df1e27d204896b2567bb9475df
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-24 12:06:32 +0600

    True and false.cpp

---
+++ True and false.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+
+    {
+        int question, answer;
+        cin >> question >> answer;
+        cout << question - answer << endl;
+    }
+    return 0;
+}
