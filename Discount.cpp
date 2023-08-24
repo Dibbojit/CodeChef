a8312f9e12e6826d899fe23031c2544606c13f97
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-24 12:23:23 +0600

    Discount.cpp

---
+++ Discount.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+
+    {
+        int discount;
+        cin >> discount;
+        discount = 100 - discount;
+        cout << discount << endl;
+    }
+    return 0;
+}
