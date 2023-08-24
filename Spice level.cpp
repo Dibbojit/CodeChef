f3153f08563eda20a257f4275316723646250833
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-08-24 11:27:21 +0600

    Spice level.cpp

---
+++ Spice level.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int spice;
+        cin >> spice;
+        if(spice < 4)
+        {
+            cout << "MILD" << endl;
+        }
+        else if(spice >= 4 && spice < 7)
+        {
+            cout << "MEDIUM" << endl;
+        }
+        else if(spice >= 7)
+        {
+            cout << "HOT" << endl;
+        }
+    }
+    return 0;
+}
