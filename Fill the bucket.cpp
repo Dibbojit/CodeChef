9b6239a6d28aeaca0fdac2e8542b7493f893c171
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-09-05 10:18:29 +0600

    Fill the bucket.cpp

---
+++ Fill the bucket.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        int k, x;
+        cin >> k >> x;
+        cout << k - x << endl;
+    }
+    return 0;
+}
