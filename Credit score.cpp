86275f0673dc471c2d120e46c48bcdf61b2266c8
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-09-05 16:25:01 +0600

    Credit score.cpp

---
+++ Credit score.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+     int score;
+     cin >> score;
+     if(score >= 750)
+     {
+        cout << "YES" << endl;
+     }
+     else 
+     {
+        cout << "NO" << endl;
+     }
+    
+}
