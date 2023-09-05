51fa3c7c0980516f36ac7f3c17cfc483ae3bf5a7
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-09-05 12:06:32 +0600

    AI analysis code.cpp

---
+++ AI analysis code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c;
+    cin >> c;
+    if(c <= 1000)
+    {
+        cout << "Yes" << endl;
+    }
+    else 
+    {
+        cout << "No" << endl;
+    }
+}
