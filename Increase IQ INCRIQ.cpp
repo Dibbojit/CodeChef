d4b968336ff2b18057d0bcbcf8e672ac17842702
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-09-07 12:29:53 +0600

    Increase IQ INCRIQ.cpp

---
+++ Increase IQ INCRIQ.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+        int x;
+        cin >> x;
+        x = x + 7;
+        if(x > 170)
+        {
+            cout << "YES" << endl;
+        }
+        else 
+        {
+            cout << "NO" << endl;
+        }
+    
+    return 0;
+}
